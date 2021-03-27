b6f755e60d8e9ad43f682c6ad7062a47e7e97c11
Kenan Gündoğan <kenangundogann@gmail.com>
2021-03-27 17:21:25 +0300

    Create webpack.common.js

---
+++ webpack.common.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+	context: path.resolve(__dirname, 'src'),
+	entry: {
+		madosel: './app.scss',
+	},
+	output: {
+		path: path.join(__dirname, 'dist'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: "css-loader",
+						options: {
+							sourceMap: true
+						},
+					},
+					{
+						loader: 'postcss-loader',
+						options: {
+							sourceMap: true
+						},
+					},
+					{
+						loader: "sass-loader",
+						options: {
+							sourceMap: true,
+							sassOptions: {
+								// outputStyle: "compressed",
+							},
+						},
+					},
+				],
+			},
+		],
+	}
+};
