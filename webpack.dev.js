76a305a81cdbda3e546119b60ed9829cfb8df8b5
Kenan Gundogan <kenangundogann@gmail.com>
2021-04-25 10:53:53 +0300

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,14 +1,22 @@
+const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+
 module.exports = merge(common, {
-	mode: 'development',
-	devtool: 'source-map',
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: 'css/[name].css',
-			chunkFilename: 'css/[id].css',
-		}),
-	],
+    mode: 'development',
+    devtool: 'source-map',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'js/[name].js',
+        // clean: true
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].css',
+            chunkFilename: 'css/[id].css',
+        }),
+        new RemoveEmptyScriptsPlugin()
+    ],
 });
