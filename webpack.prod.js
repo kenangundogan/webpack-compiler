eeeadb38c2ada826f7af68d724dd9911f8005fb4
Kenan Gundogan <kenangundogann@gmail.com>
2021-04-25 10:54:01 +0300

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,21 @@
+const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+
 module.exports = merge(common, {
 	mode: 'production',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'js/[name].min.js',
+        // clean: true
+    },
 	plugins: [
 		new MiniCssExtractPlugin({
 			filename: 'css/[name].min.css',
 			chunkFilename: 'css/[id].min.css',
 		}),
+        new RemoveEmptyScriptsPlugin()
 	],
 });
